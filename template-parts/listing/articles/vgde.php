b7e87d31e809370855d9b2531ca7b45dcd087e00
Honza Bambas :mayhemer <honzab@firemni.cz>
2019-03-31 19:24:18 +0200

    responsive vgde homepage

--- template-parts/listing/articles/vgde.php
+++ template-parts/listing/articles/vgde.php
@@ -13,15 +13,20 @@ $image = Images::get_post_image($post_order_in_category == 0 ? 'full_width' : 'm
 
 <article class="article-grid">
 <a class="article-permalink" href="<?php the_permalink();?>">
-  <img src="<?php echo esc_url($image['image']); ?>">
-  <div class="article-grid__content">
-    <header>
-      <h2 class="article-grid__heading">
-        <?php esc_html(the_title());?>
-      </h2>
-    </header>
-  </div>
-</a>
+<div class="article-grid-wrapper">
 
+<img src="<?php echo esc_url($image['image']); ?>">
+
+<div class="article-grid__content">
+  <header>
+    <h2 class="article-grid__heading">
+      <?php esc_html(the_title());?>
+    </h2>
+    <div class="article-grid__underline"></div>
+  </header>
+</div>
+
+</div>
+</a>
 <?php require locate_template('template-parts/parts/google-rich-snippets.php');?>
 </article>
