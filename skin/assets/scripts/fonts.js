48ed4d0e9a42c0ff9df8bfb69ed995bebb14eab4
Honza Bambas :mayhemer <honzab@firemni.cz>
2019-04-01 03:35:06 +0200

    big font play on the sunday night :)

--- skin/assets/scripts/fonts.js
+++ skin/assets/scripts/fonts.js
@@ -5,8 +5,17 @@ import WebFont from 'webfontloader';
 WebFont.load({
   google: {
     families: [
-      'Open Sans:400:latin-ext',
-      'Forum:400:latin-ext'
+      'Open Sans:400',
+      'Open Sans:300',
+      'Open Sans Condensed:300',
+      'Gafata:300',
+      'Noto Serif KR:300',
+      //'Prata',
+      //'Antic Didone',
+      'Oranienbaum',
+      'Assistant:300',
+      'Noto Serif TC:200',
+      'Rasa',
     ],
   },
 });
