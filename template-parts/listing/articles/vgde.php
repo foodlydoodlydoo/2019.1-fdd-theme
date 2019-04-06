be8bd5abb63e6293d321b5028b2a9d0b558be933
Honza Bambas :mayhemer <honzab@firemni.cz>
2019-04-06 21:39:10 +0200

    REMOVE/TESTING ONLY (random height of images in .oldish, article order in markup)

--- template-parts/listing/articles/vgde.php
+++ template-parts/listing/articles/vgde.php
@@ -15,12 +15,20 @@ $image = Images::get_post_image($post_order_in_category == 0 ? 'full_width' : 'm
 <a class="article-permalink" href="<?php the_permalink();?>">
 <div class="article-grid-wrapper">
 
-<img src="<?php echo esc_url($image['image']); ?>">
+<img src="<?php echo esc_url($image['image']); ?>" style="<?php
+if ($post_order_in_category > 0) {
+  $_height = random_int(250, 500);
+  echo "min-height: " . $_height . "px;";
+  echo "max-height: " . $_height . "px;";
+  echo "height: " . $_height . "px;";
+}
+?>">
 
 <div class="article-grid__content">
   <header>
     <h2 class="article-grid__heading">
-      <?php esc_html(the_title());?>
+      <?php esc_html(the_title());
+print("[$post_order_in_category]");?>
     </h2>
     <div class="article-grid__excert">
       <?php the_excerpt();?>
