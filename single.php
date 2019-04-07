b6e1d025da99d57d13939627ae9f66655775b0f2
Honza Bambas :mayhemer <honzab@firemni.cz>
2019-04-07 19:32:05 +0200

    auto reformat

--- single.php
+++ single.php
@@ -7,10 +7,10 @@
 
 get_header();
 
-if ( have_posts() ) {
-  while ( have_posts() ) {
+if (have_posts()) {
+  while (have_posts()) {
     the_post();
-    get_template_part( 'template-parts/single/post' );
+    get_template_part('template-parts/single/post');
   }
 }
 
--- template-parts/single/page.php
+++ template-parts/single/page.php
@@ -7,19 +7,19 @@
 
 use Fdd\Theme\Utils\Images;
 
-$image = Images::get_post_image( 'full_width' );
+$image = Images::get_post_image('full_width');
 ?>
 
 <!-- Single Content Section -->
-<section class="single" id="<?php echo esc_attr( $post->ID ); ?>">
-  <div class="single__image" style="background-image: url('<?php echo esc_url( $image['image'] ); ?>');"></div>
+<section class="single" id="<?php echo esc_attr($post->ID); ?>">
+  <div class="single__image" style="background-image: url('<?php echo esc_url($image['image']); ?>');"></div>
   <header>
     <h1 class="single__title">
-      <?php the_title(); ?>
+      <?php the_title();?>
     </h1>
   </header>
   <div class="single__content content-style content-media-style">
-    <?php the_content(); ?>
+    <?php the_content();?>
   </div>
-  <?php require locate_template( 'template-parts/parts/google-rich-snippets.php' ); ?>
+  <?php require locate_template('template-parts/parts/google-rich-snippets.php');?>
 </section>
