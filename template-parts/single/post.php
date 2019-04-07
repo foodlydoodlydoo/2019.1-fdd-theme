0ad66ef6484a82188d6c9c46b8be9c1a18abd226
Honza Bambas :mayhemer <honzab@firemni.cz>
2019-04-07 19:32:49 +0200

    add categories to class names, comment out the invisible lazy loaded featured image

--- template-parts/single/post.php
+++ template-parts/single/post.php
@@ -5,21 +5,33 @@
  * @package Fdd\Template_Parts\Single
  */
 
+/*
 use Fdd\Theme\Utils\Images;
 
-$image = Images::get_post_image( 'full_width' );
+$image = Images::get_post_image('full_width');
+ */
+
+function extract_category_slugs($accumulator, $entry) {
+  if ($entry->taxonomy == 'category') {
+    $accumulator = array_merge($accumulator, array('category-' . $entry->slug));
+  }
+  return $accumulator;
+}
+
+$category_slugs = join(' ', array_reduce(get_the_category(), 'extract_category_slugs', array()));
+
 ?>
 
 <!-- Single Content Section -->
-<section class="single" id="<?php echo esc_attr( $post->ID ); ?>">
-  <div class="single__image" data-normal="<?php echo esc_url( $image['image'] ); ?>"></div>
+<section class="single <?php echo $category_slugs; ?>" id="<?php echo esc_attr($post->ID); ?>">
+  <!--div class="single__image" data-normal="<?php echo esc_url($image['image']); ?>"></div-->
   <header>
     <h1 class="single__title">
-      <?php the_title(); ?>
+      <?php the_title();?>
     </h1>
   </header>
   <div class="single__content content-style content-media-style">
-    <?php the_content(); ?>
+    <?php the_content();?>
   </div>
-  <?php require locate_template( 'template-parts/parts/google-rich-snippets.php' ); ?>
+  <?php require locate_template('template-parts/parts/google-rich-snippets.php');?>
 </section>
