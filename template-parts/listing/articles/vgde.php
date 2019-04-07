08e7bca3bbda138df29ef76ec49135e4d24cc19c
Honza Bambas :mayhemer <honzab@firemni.cz>
2019-04-07 14:58:50 +0200

    homepage mix blanding hover, excerpt super-narrow responsiveness

--- template-parts/listing/articles/vgde.php
+++ template-parts/listing/articles/vgde.php
@@ -15,14 +15,16 @@ $image = Images::get_post_image($post_order_in_category == 0 ? 'full_width' : 'm
 <a class="article-permalink" href="<?php the_permalink();?>">
 <div class="article-grid-wrapper">
 
+<div class="article-grid__figure">
 <img src="<?php echo esc_url($image['image']); ?>" style="<?php
-if ($post_order_in_category > 0) {
+if (0 && $post_order_in_category > 0) {
   $_height = random_int(250, 500);
   echo "min-height: " . $_height . "px;";
   echo "max-height: " . $_height . "px;";
   echo "height: " . $_height . "px;";
 }
 ?>">
+</div>
 
 <div class="article-grid__content">
   <header>
@@ -30,7 +32,7 @@ if ($post_order_in_category > 0) {
       <?php esc_html(the_title());
 print("[$post_order_in_category]");?>
     </h2>
-    <div class="article-grid__excert">
+    <div class="article-grid__excerpt">
       <?php the_excerpt();?>
     </div>
     <div class="article-grid__underline"></div>
