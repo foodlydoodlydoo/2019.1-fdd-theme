eacb2f637db8dc67d7674e3ff77cb8209bcf2d86
Honza Bambas :mayhemer <honzab@firemni.cz>
2019-04-28 18:23:08 +0200

    footer styling and menu

--- template-parts/footer/content.php
+++ template-parts/footer/content.php
@@ -5,10 +5,14 @@
  * @package Fdd\Template_Parts\Footer
  */
 
+use Fdd\Admin\Menu\Menu;
+
+$footer_menu = new Menu();
 ?>
 
 <footer class="footer">
   <div class="footer__container">
-  This is about to be at the bottom of the page.
+  © FoodlyDoodlyDoo 2019
+  <?php echo esc_html($footer_menu->bem_menu('footer_main_nav', 'footer-navigation')); ?>
   </div>
 </footer>
