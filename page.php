fe1b4d1b252743cfb7014ac54c4a64ab09593d42
Honza Bambas :mayhemer <honzab@firemni.cz>
2019-05-27 12:45:49 +0200

    tseting code in page.php for the import filter (artifical)

--- page.php
+++ page.php
@@ -15,3 +15,20 @@ if (have_posts()) {
 }
 
 get_footer();
+
+if (true) {
+  $str = file_get_contents('test-vid.txt');
+
+  fdd_reg_shortcodes();
+  $str = do_shortcode($str);
+  print_r($str);
+
+  $post = array('post_content' => $str, 'post_type' => 'post', 'terms' => array(
+    array('domain' => 'category', 'slug' => 'recipes'),
+  ));
+  $post = fdd_filter_wp_import_post_data_raw($post);
+
+  file_put_contents('test.out', $post['post_content']);
+
+  echo "\nSuccess!\n";
+}
