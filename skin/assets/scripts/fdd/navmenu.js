5d910c151250b6b8f8cbf8139231698649f3d269
Honza Bambas :mayhemer <honzab@firemni.cz>
2019-06-09 00:00:21 +0200

    menu responsivitiy tuned

--- skin/assets/scripts/fdd/navmenu.js
+++ skin/assets/scripts/fdd/navmenu.js
@@ -2,12 +2,12 @@ export class FDD_FullscreenMenu {
   constructor(item, target, onRevealed, onHidden) {
     const menu = $(target);
     const close = () => {
-      menu.css('height', '0');
+      menu.removeClass('open');
       onHidden(menu);
     };
 
     $(`.header .main-navigation .main-navigation__item--${item} > a`).click(() => {
-      menu.css('height', '100%');
+      menu.addClass('open');
       onRevealed(menu, close);
     });
     $(`${target}__close-button`).click(close);
--- template-parts/header/header.php
+++ template-parts/header/header.php
@@ -16,13 +16,13 @@ $logo_img = General_Helper::get_manifest_assets_data('foodly-doodly-doo-logo-m.p
 
 ?>
 <div id="fullscreen-menu">
-<span id="fullscreen-menu__close-button">&#x2573;</span>
+<img class="fullscreen-menu__logo-img" src="<?php echo esc_url($logo_img); ?>">
+<span id="fullscreen-menu__close-button">&times;</span>
 <?php echo esc_html($main_menu->bem_menu('fullscreen_nav', 'fs-navigation')); ?>
 </div>
 <div class="header">
   <a class="header__logo-link" href="<?php echo esc_url(home_url()); ?>" title="<?php echo esc_attr($blog_name); ?>">
     <img class="header__logo-img" src="<?php echo esc_url($logo_img); ?>" title="<?php echo esc_attr($header_logo_info); ?>" alt="<?php echo esc_attr($header_logo_info); ?>" />
-    <!--div class="header__logo">FOODLYdoodlyDoo.com</div-->
   </a>
 <?php echo esc_html($main_menu->bem_menu('header_main_nav', 'main-navigation')); ?>
 </div>
