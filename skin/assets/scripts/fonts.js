f035e32ae630b4cf2153804905ebac6dee0da1e9
Honza Bambas :mayhemer <honzab@firemni.cz>
2019-07-13 14:52:55 +0200

    npm install @XXX according infinum current package.json / linting fixes

--- skin/assets/scripts/fonts.js
+++ skin/assets/scripts/fonts.js
@@ -2,22 +2,20 @@
 
 import WebFont from 'webfontloader';
 
+/** unused */
+
+// 'Open Sans:300',
+// 'Open Sans Condensed:300',
+// 'Prata',
+// 'Antic Didone',
+// 'Assistant:300',
+// 'Noto Serif TC:200',
+// 'Rasa',
+// 'Gafata:400',
+
 WebFont.load({
   google: {
     families: [
-      /** unused */
-
-      //'Open Sans:300',
-      //'Open Sans Condensed:300',
-      //'Prata',
-      //'Antic Didone',
-      //'Assistant:300',
-      //'Noto Serif TC:200',
-      //'Rasa',
-      // 'Gafata:400',
-
-      /** used */
-
       'Noto Serif KR:300',
       'Spectral:200i',
       'Abel:400',
