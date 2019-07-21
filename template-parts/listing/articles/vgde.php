874d260bb11853001ee64080f415ae2312ab9502
Honza Bambas :mayhemer <honzab@firemni.cz>
2019-07-21 20:46:16 +0200

    excerpt 30 words

--- template-parts/listing/articles/vgde.php
+++ template-parts/listing/articles/vgde.php
@@ -12,7 +12,7 @@ $image = Images::get_post_image($post_order_in_category == 0
   ? 'fdd:listing:first-article'
   : 'fdd:listing:oldish-article');
 
-$excerpt = Excerpt::get_excerpt(get_the_excerpt(), 22);
+$excerpt = Excerpt::get_excerpt(get_the_excerpt(), 30);
 
 $grid_classes = ["article-grid"];
 if ($is_first_post) array_push($grid_classes, "first-article");
