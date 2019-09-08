8b618b4aded912ca3b43a81fac6d5e99057a67c5
Honza Bambas :mayhemer <honzab@firemni.cz>
2019-09-08 16:16:05 +0200

    removed unwanted meta tags from <head>

--- template-parts/header/head.php
+++ template-parts/header/head.php
@@ -18,9 +18,6 @@ $logo_img = General_Helper::get_manifest_assets_data('images/logo.svg');
 <!-- Remove IE's ability to use compatibility mode -->
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
-<!-- Correct type -->
-<meta http-equiv="Content-type" content="text/html; charset=utf-8">
-
 <!-- Disable phone formatin on safari -->
 <meta name="format-detection" content="telephone=no">
 
@@ -40,7 +37,7 @@ $logo_img = General_Helper::get_manifest_assets_data('images/logo.svg');
 <meta name="apple-mobile-web-app-title" content="<?php echo esc_attr(get_bloginfo('name')); ?>">
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="apple-mobile-web-app-status-bar-style" content="#C3151B">
-<link rel="apple-touch-startup-image" href="<?php echo esc_url($logo_img); ?>">
+<!--link rel="apple-touch-startup-image" href="<?php echo esc_url($logo_img); ?>"-->
 
 <?php
 get_template_part('template-parts/tracking/head');
