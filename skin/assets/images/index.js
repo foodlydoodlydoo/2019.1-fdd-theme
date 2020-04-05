675c511cdb151d2d62173c6c3a8240152c233868
Honza Bambas :mayhemer <honzab@firemni.cz>
2020-04-05 22:15:24 +0200

    logo in the footer

--- skin/assets/images/index.js
+++ skin/assets/images/index.js
@@ -1,4 +1,5 @@
 import './foodly-doodly-doo-logo.png';
 import './foodly-doodly-doo-logo-m.png';
+import './foodly-doodly-doo-logo-mcr.png';
 import './nothning-found.png';
 import './play-button.svg';
--- template-parts/footer/content.php
+++ template-parts/footer/content.php
@@ -6,13 +6,18 @@
  */
 
 use Fdd\Admin\Menu\Menu;
+use Fdd\Helpers\General_Helper;
 
 $footer_menu = new Menu();
+$logo_img = General_Helper::get_manifest_assets_data('foodly-doodly-doo-logo-mcr.png');
 ?>
 
 <footer class="footer">
   <div class="footer__container">
-  © FoodlyDoodlyDoo 2019
-  <?php echo esc_html($footer_menu->bem_menu('footer_main_nav', 'footer-navigation')); ?>
+    <span>© FoodlyDoodlyDoo 2019</span>
+    <?php echo esc_html($footer_menu->bem_menu('footer_main_nav', 'footer-navigation')); ?>
+  </div>
+  <div class="footer__logo_container">
+    <img class="footer__logo" src="<?php echo esc_url($logo_img);?>"/>
   </div>
 </footer>
