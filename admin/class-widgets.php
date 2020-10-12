922f73a5625b440d51db3adb2b524dad680c1f26
Honza Bambas :mayhemer <honzab@firemni.cz>
2020-10-12 13:33:32 +0200

    shop CtAs widgets

--- admin/class-widgets.php
+++ admin/class-widgets.php
@@ -21,11 +21,22 @@ class Widgets {
   public function register_widget_position() {
     register_sidebar(
       array(
-          'name'          => esc_html__( 'Blog', 'fdd' ),
-          'id'            => 'blog',
-          'description'   => esc_html__( 'Description', 'fdd' ),
-          'before_widget' => '',
-          'after_widget'  => '',
+          'name'          => esc_html__( 'Shop ad on the Homepage', 'fdd' ),
+          'id'            => 'shop-home-call-to-action',
+          'description'   => esc_html__( 'This is what we show on the home page betwean Recipe and Food art sections', 'fdd' ),
+          'before_widget' => '<div class="home-call-to-action--wrap"><div class="home-call-to-action shop">',
+          'after_widget'  => '</div></div>',
+          'before_title'  => '',
+          'after_title'   => '',
+      )
+    );
+    register_sidebar(
+      array(
+          'name'          => esc_html__( 'Shop ad in Recipes, Food Art pages', 'fdd' ),
+          'id'            => 'shop-pages-call-to-action',
+          'description'   => esc_html__( 'This is what we show in individual recipes/art pages', 'fdd' ),
+          'before_widget' => '<div class="single-call-to-action--wrap"><div class="single-call-to-action shop">',
+          'after_widget'  => '</div></div>',
           'before_title'  => '',
           'after_title'   => '',
       )
