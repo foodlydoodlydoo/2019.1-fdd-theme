dfda32de07c4538e634524203b4c94548d958b99
Honza Bambas :mayhemer <honzab@firemni.cz>
2020-10-12 14:45:21 +0200

    shoppipng cart image for the main menu

--- skin/assets/images/index.js
+++ skin/assets/images/index.js
@@ -3,3 +3,5 @@ import './foodly-doodly-doo-logo-m.png';
 import './foodly-doodly-doo-logo-mcr.png';
 import './nothning-found.png';
 import './play-button.svg';
+import './search.svg';
+import './shopping-cart.svg';
