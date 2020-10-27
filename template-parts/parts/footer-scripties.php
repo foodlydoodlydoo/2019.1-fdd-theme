9e0493ce92b1799d68f1ac4fb33a4ff4cc353a5c
Honza Bambas :mayhemer <honzab@firemni.cz>
2020-10-27 13:46:56 +0100

    in-image product CtA dyn script, based on the core/image newly added attribute

--- template-parts/parts/footer-scripties.php
+++ template-parts/parts/footer-scripties.php
@@ -1,8 +1,32 @@
+<?php
+
+$product_base = 'product';
+if (function_exists('wc_get_permalink_structure')) {
+  $permalink = wc_get_permalink_structure();
+  $product_base = $permalink['product_rewrite_slug'];
+}
+
+$product_image_cta = \FDD\Core\get_custom_content('image_inline_product_cta');
+
+?>
 <script>
-jQuery(document).ready(function() {
-  jQuery("img").on("contextmenu",function(event) {
-    event.preventDefault();
-    return false;
+jQuery(document).ready(function($) {
+  $("img").on("contextmenu", function(e) { e.preventDefault(); return false;});
+  $("figure.wp-block-image[productslug]:not([productslug=\"\"])").each(function() {
+    const figure = $(this);
+    const link = figure.find('a');
+    const productslug = figure.attr('productslug');
+    const productlink = `/<?php echo $product_base; ?>/${productslug}`;
+    const element = $("<a>")
+      .prop("href", productlink)
+      .addClass("fdd-product-link")
+      .click(event => {
+        window.location.href = productlink;
+        event.preventDefault();
+        return false;
+      })
+      .append($("<span>").text('<?php echo $product_image_cta; ?>'));
+    link.append(element);
   });
 });
 </script>
